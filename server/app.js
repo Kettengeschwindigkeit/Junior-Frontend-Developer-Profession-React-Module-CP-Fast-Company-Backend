e3d2c5873bfa62e37515ffa8a9a23fc4e2ff01ff
Kettengeschwindigkeit <666damned999@mail.ru>
2022-11-29 13:19:26 +0300

    5. Backend. Part I. Creating routes structure

--- server/app.js
+++ server/app.js
@@ -2,12 +2,15 @@ const express = require('express')
 const mongoose = require('mongoose')
 const config = require('config')
 const chalk = require('chalk')
-const initDatabase = require('./startUp/initDatabase') 
+const initDatabase = require('./startUp/initDatabase')
+const routes = require('./routes')
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+app.use('/api', routes)
 
 const PORT = config.get('port') ?? 8080
 
