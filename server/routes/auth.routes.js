39b6ee22217717b83c1ef652c29bdb36da5f815d
Kettengeschwindigkeit <666damned999@mail.ru>
2022-11-30 17:54:16 +0300

    6. Backend. Part II. Authorization

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -57,9 +57,56 @@ router.post('/signUp', [
     }
 ])
 
-router.post('/signInWithPassword', async (req, res) => {
+router.post('/signInWithPassword', [
+    check('email', 'Incorrect email').normalizeEmail().isEmail(),
+    check('password', 'Password cannot be empty').exists(),
+    async (req, res) => {
+        try {
+            const errors = validationResult(req)
 
-})
+            if (!errors.isEmpty()) {
+                return res.status(400).json({
+                    error: {
+                        message: 'INVALID_DATA',
+                        code: 400,
+                    }
+                })
+            }
+
+            const { email, password } = req.body
+
+            const existingUser = await User.findOne({ email })
+
+            if (!existingUser) {
+                return res.status(400).send({
+                    error: {
+                        message: 'EMAIL_NOT_FOUND',
+                        code: 400
+                    }
+                })
+            }
+
+            const isPasswordEqual = await bcrypt.compare(password, existingUser.password)
+
+            if (!isPasswordEqual) {
+                return res.status(400).send({
+                    error: {
+                        message: 'INVALID_PASSWORD',
+                        code: 400
+                    }
+                })
+            }
+
+            const tokens = tokenService.generate({ _id: existingUser._id })
+            await tokenService.save(existingUser._id, tokens.refreshToken)
+
+            res.status(201).send({ ...tokens, userId: existingUser._id })
+        } catch (e) {
+            res.status(500).json({
+                message: 'Server error. Try it later'
+            })
+        }
+}])
 
 router.post('/token', async (req, res) => {
 
