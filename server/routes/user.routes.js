812148e729a8ad39ea5c501b5e3c8b68a80bc8f6
Kettengeschwindigkeit <666damned999@mail.ru>
2022-12-01 16:58:48 +0300

    7. Backend. Part III. Users logic

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,4 +1,33 @@
 const express = require('express')
+const User = require('../models/User')
 const router = express.Router({ mergeParams: true })
+
+router.patch('/:userId', async (req, res) => {
+    try {
+        const { userId } = req.params
+
+        if (userId) {
+            const updatedUser = await User.findByIdAndUpdate(userId, req.body, { new: true })
+            res.send(updatedUser)
+        } else {
+            res.status(401).json({ message: 'Unauthorized' })
+        }
+    } catch (e) {
+        res.status(500).json({
+            message: 'Server error. Try it later'
+        })
+    }
+})
+
+router.get('/', async (req, res) => {
+    try {
+        const list = await User.find()
+        res.send(list)
+    } catch (e) {
+        res.status(500).json({
+            message: 'Server error. Try it later'
+        })
+    }
+})
 
 module.exports = router
